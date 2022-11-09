9848e093ea292b3139b7efd659e361b1e79d91c6
Bobby <bobbyrayrod92@gmail.com>
2022-11-09 12:48:59 -0700

    B- fixed function for calculating distance

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,8 @@ console.log(addressData);
 
 function getDistance(x1, y1, x2, y2){
     let y = x2 - x1;
-    let x = y2 - x1;
+    let x = y2 - y1;
 
     return Math.sqrt(x * x + y * y);
-    
+
 }
