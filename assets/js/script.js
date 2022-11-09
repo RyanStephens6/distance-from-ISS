b84dceab7d112357ccb318d8690a9c63a401d5d3
[Andres Perez] <mandresperez.uk@gmail.com>
2022-11-09 13:05:25 -0700

    pull from repo

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,12 +23,3 @@ var addressData = fetch(addressLink, {
 
 console.log(issData);
 console.log(addressData);
-
-
-function getDistance(x1, y1, x2, y2){
-    let y = x2 - x1;
-    let x = y2 - x1;
-
-    return Math.sqrt(x * x + y * y);
-    
-}
