65ed459c2f99ec4171f156f00da20dc0cac415bc
[Andres Perez] <mandresperez.uk@gmail.com>
2022-11-09 13:03:22 -0700

    updated local files

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,12 @@ var addressData = fetch(addressLink, {
 
 console.log(issData);
 console.log(addressData);
+
+
+function getDistance(x1, y1, x2, y2){
+    let y = x2 - x1;
+    let x = y2 - x1;
+
+    return Math.sqrt(x * x + y * y);
+    
+}
