beeabec417718cf9d20e17ab27da065376a38027
Ryan Stephens <ryandstephens6@gmail.com>
2022-11-10 11:42:49 -0700

    Add function to update the distance container based on the users address input

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,13 +32,30 @@ function handleSubmitButton() {
     }
     addressUrl += "&apiKey=76f8a5221fbe49a7b156d4fddcaeeaad";
     getAddressCoordinates(addressUrl);
+    updateDistanceContainer();
 }
 
+//(x1, y1) = (latitude, longitude) ISS coordinates and home address can be interchanged
 function getDistance(x1, y1, x2, y2) {
   let y = x2 - x1;
   let x = y2 - y1;
 
   return Math.sqrt(x * x + y * y);
+}
+
+function updateDistanceContainer() {
+    var issCoordinates = document.getElementById("iss-coordinates").textContent;
+    var issArr = issCoordinates.split(' ');
+    var issX = issArr[0];
+    var issY = issArr[1];
+    
+    var addressCoordinates = document.getElementById("address-coordinates").textContent;
+    var addressArr = addressCoordinates.split(' ');
+    var addressX = addressArr[0];
+    var addressY = addressArr[1];
+
+    var distance = getDistance(issX, issY, addressX, addressY);
+    document.getElementById("distance").value = distance;
 }
 
 submitButton.addEventListener("click", handleSubmitButton)
